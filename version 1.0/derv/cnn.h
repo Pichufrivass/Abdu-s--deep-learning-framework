a921f8887022888fb393fcc1b82182ad510578f8
Pichufrivass <0281551@up.edu.mx>
2026-08-18 08:40:47 -0600

    Add files via upload

---
+++ version 1.0/derv/cnn.h
@@ -0,0 +1,18 @@
+#ifndef CNN_H
+#define CNN_H
+#include<iostream>
+#include<vector>
+#include<memory>
+#include<set>
+using namespace std;
+
+class filtro{
+public:
+	neuronas[3][3];
+	static random_device rd;
+	static mt19937 gen(rd());
+	uniform_real_distribution<double> dis_w(0.1, 1.0);
+	for (int i = 0; i < 9; i++){
+		dis_w(gen)));
+	}
+}
